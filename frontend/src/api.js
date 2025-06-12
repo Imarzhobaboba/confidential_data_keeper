0aab250b2fdc921c5b9b451a89fdadce3c7c7216
user <filia150@mail.ru>
2025-06-12 17:35:53 +0300

    now frontend connects to localhost:8000 instead of backend:8000 and now frontend can connect to backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/secret';
+const API_BASE_URL = 'http://localhost:8000/secret';
 
 // Создаем экземпляр axios с базовыми настройками
 const api = axios.create({
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -7,12 +7,10 @@ export default defineConfig({
     host: true,
     port: 5173,
     strictPort: true,
-    headers: {
-      "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:"
-    }
+    headers: {},
   },
   build: {
     target: 'esnext',
     minify: true
   }
-})
+});
